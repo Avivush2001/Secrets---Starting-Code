3374cbc65d82f6c28d80e2c69cf0786897ac8350
Avivush2001 <avivbarak01@gmail.com>
2023-02-27 19:46:06 +0200

    Added google sign up/in

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const _ = require("lodash")
 const session = require("express-session")
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose")
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const findOrCreate = require('mongoose-findorcreate')
 
 main().catch(err => console.log(err));
 
@@ -35,16 +37,48 @@ async function main() {
     // user schema setup
     const userSchema = new mongoose.Schema({
         email: String,
-        password: String
+        password: String,
+        googleId: String
     })
     userSchema.plugin(passportLocalMongoose)
+    userSchema.plugin(findOrCreate)
 
     // setup mongoose model
     const User = new mongoose.model("user", userSchema)
 
+    //passport setups
     passport.use(User.createStrategy())
-    passport.serializeUser(User.serializeUser())
-    passport.deserializeUser(User.deserializeUser())
+    // passport.serializeUser(User.serializeUser())
+    // passport.deserializeUser(User.deserializeUser())
+    passport.serializeUser(function(user, done){
+
+      done(null, user.id);
+    
+    });
+    
+    
+    passport.deserializeUser(function(id, done){
+    
+      User.findById(id, function(err, user){
+    
+        done(err, user);
+    
+      });
+    
+    });
+
+    passport.use(new GoogleStrategy({
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    }, (acessToken, refreshToken, profile, cb) => {
+      console.log(profile)
+      User.findOrCreate({ googleId: profile.id}, (err, user) => {
+        return cb(err, user)
+      })
+    }))
+
     // get setups
     app.get("/", function(req, res) {
         res.render("home")
@@ -57,6 +91,7 @@ async function main() {
     app.get("/register", function(req, res) {
         res.render("register")
     })
+
     app.get("/secrets", function(req, res) {
         // The below line was added so we can't display the "/secrets" page
         // after we logged out using the "back" button of the browser, which
@@ -70,10 +105,19 @@ async function main() {
             res.render("secrets")
         } else res.redirect("/login")
     })
+
     app.get("/logout", function(req, res) {
         req.logout(function(err) {console.log(err)})
-        res.redirect("/")
+        res.redirect("/secrets")
     })
+
+    app.get("/auth/google", passport.authenticate('google', {scope: ['email', 'profile']}));
+
+    app.get("/auth/google/secrets", passport.authenticate('google', {failureRedirect: '/login'}),
+    function(req, res) {
+      res.redirect('/secrets')
+    })
+
     // post setups
     app.post("/register", function(req, res) {
         User.register({username: req.body.username}, req.body.password, function(err, user) {
